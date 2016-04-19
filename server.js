34ff73f4e36db65f1f03474f101137124c40a3b7
Nathan Meyers <nathan@runnable.com>
2016-04-18 19:23:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ console.log('id:', id, 'port', port);
 primus.on("connection", function (spark) {
   console.log('connected = ' + id);
   setInterval(function() {
-    spark.write("I AM A BRANCH ID: " + id);
+    spark.write("I AM BRANCH ID: " + id);
   }, 1000);
 });
